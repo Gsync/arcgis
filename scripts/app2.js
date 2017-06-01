a89463caf566e9ffaec340d30550770dc53c81b1
Khuram <knshaikh@gmail.com>
2017-06-01 16:53:45 -0600

    added search widget

---
+++ scripts/app2.js
@@ -0,0 +1,41 @@
+ require([
+        "esri/map",
+        "esri/dijit/Search",
+        "esri/geometry/Extent",
+        "esri/graphic",
+        "esri/symbols/SimpleMarkerSymbol",
+        "esri/geometry/screenUtils",
+        "dojo/dom",
+        "dojo/dom-construct",
+        "dojo/query",
+        "dojo/_base/Color",
+        "dojo/domReady!"
+      ], function (Map, Search, Extent, Graphic, SimpleMarkerSymbol, screenUtils, dom, domConstruct, query, Color) {
+  // create a map and instance of the search widget here
+  var map = new Map("map", {
+      basemap: "topo",
+      center: [-117.19, 34.055],
+      zoom: 15
+    });
+
+    var search = new Search({
+       map: map
+    }, "search");
+
+    //Create extent to limit search
+      var extent = new Extent({
+         "spatialReference": {
+            "wkid": 102100
+            },
+         "xmin": -13063280,
+            "xmax": -13033928,
+            "ymin": 4028345,
+            "ymax": 4042715
+         });
+
+    //set the source's searchExtent to the extent specified above
+      search.sources[0].searchExtent = extent;
+
+    //don't forget to start the widget
+    search.startup();
+});
