a94d9c2cf769a0cb64511bcb0ed44ed436b511c6
Khuram <knshaikh@gmail.com>
2017-06-04 14:14:03 -0600

    draw polygon to select and query

--- scripts/featurelayer.js
+++ scripts/featurelayer.js
@@ -2,8 +2,10 @@ require([
   'dojo/dom',
   'dojo/on',
   'esri/map',
-  'esri/layers/FeatureLayer'
-], function(dom, on, Map, FeatureLayer) {
+  'esri/layers/FeatureLayer',
+  'esri/toolbars/draw',
+  'esri/tasks/query'
+], function(dom, on, Map, FeatureLayer, Draw, Query) {
   var map = new Map('map', {
     basemap: 'streets',
     autoResize: true,
@@ -11,11 +13,27 @@ require([
     zoom: 10
   });
 
-  var featureLayer = new FeatureLayer('http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/la_county_labor_centroid/FeatureServer/0');
+  var featureLayer = new FeatureLayer('http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/la_county_labor_centroid/FeatureServer/0', {
+    mode: FeatureLayer.MODE_SELECTION
+  });
 
-  featureLayer.setDefinitionExpression('TOTAL_POP > 2500');
+  drawToolbar = new Draw(map); //Instantiates Draw toolbar by providing instance of map
+
+  //Listens for Draw toolbar to finish drawing and returns an event with geometry
+  drawToolbar.on('draw-end', function(e){
+  drawToolbar.deactivate();
+  var query = new Query();
+  query.geometry = e.geometry;
+  featureLayer.selectFeatures(query); //Selects features in FeatureLayer using drawn geometry
+  });
+
+  //featureLayer.setDefinitionExpression('TOTAL_POP > 5000');
 
   map.addLayer(featureLayer);
+
+  on(dom.byId('drawPolygon'), 'click', function() {
+    drawToolbar.activate(Draw.POLYGON);
+  });
 
   on(dom.byId('population'), 'change', function(e) {
     var population = e.target.value;
